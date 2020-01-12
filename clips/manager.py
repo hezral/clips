4a0289b305c5d1bb4315dda8e0e69e1f5d2ca333
Adi Hezral <hezral@gmail.com>
2020-01-13 07:04:33 +0800

    worked on clipboard manager

--- clips/manager.py
+++ clips/manager.py
@@ -22,9 +22,51 @@
 import threading
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GLib, GObject
+from gi.repository import Gtk, Gdk, GLib, GObject
+from threading import Thread
 
-class Manager():
+class ClipsManager(GObject.GObject):
     def __init__(self):
         super().__init__()
         
+        self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+
+        def clipboard_changed(self, widget):
+            html_target = Gdk.Atom.intern('text/html', False)
+
+            
+
+            print("Current clipboard offers formats: " + str(self.wait_for_targets()[1]))
+            print(self.wait_for_contents(html_target).get_data())
+            return
+
+        def start():
+            self.clipboard.connect("owner-change", clipboard_changed)
+            return
+        
+
+        self.clipboard.connect("owner-change", clipboard_changed)
+
+    # request_contents
+  	# request_image
+  	# request_rich_text
+  	# request_targets
+  	# request_text
+  	# request_uris
+
+    # wait_for_contents
+  	# wait_for_image
+  	# wait_for_rich_text
+  	# wait_for_targets
+  	# wait_for_text
+  	# wait_for_uris
+
+    # wait_is_image_available
+  	# wait_is_rich_text_available
+  	# wait_is_target_available
+  	# wait_is_text_available
+  	# wait_is_uris_available
+
+clips = ClipsManager()
+Gtk.main()
+
