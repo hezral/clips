e493c2909bac18b7e1c7c11498a7998172ac2f8d
Adi Hezral <hezral@gmail.com>
2020-02-09 13:05:36 +0800

    work on store.py

--- clips/store.py
+++ clips/store.py
@@ -25,46 +25,63 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib, GObject
 import sqlite3
 
-
-
 class ClipsStore():
     def __init__(self):
         super().__init__()
 
-        #debug flag
         debugflag = True
         
-        database_file = 'ClipsDatabase.db'
-        self.database = database_file 
+        db_file = 'ClipsDatabase.db'
 
-        def open_db(database_file):
-            connection = sqlite3.connect(database_file) 
-            cursor = connection.cursor()
-            cursor.execute("PRAGMA database_list;")
-            curr_table = cursor.fetchall()
-            print(curr_table)
-            pass
-
-        def debug():
-          self.label = Gtk.Label()
-          self.image = Gtk.Image.new_from_icon_name("image-x-generic", Gtk.IconSize.DIALOG)
-          self.window = Gtk.Window(title="Clips Debug Window") #debug window to see contents displayed in Gtk.Window
-          self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-          self.box.pack_start(self.image, True, True, 0)
-          self.box.pack_start(self.label, True, True, 0)
-          self.window.set_border_width(6)
-          self.window.add(self.box)
-          self.window.show_all()
-          self.window.connect("destroy", Gtk.main_quit)
-          # just for debugging at CLI to enable CTRL+C quit
-          GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, signal.SIGINT, Gtk.main_quit)
+        try:
+            db_connection, db_cursor = self.open_db(db_file)
+            self.create_table(db_cursor)
+        except sqlite3.Error as error:
+            print("Error while working with SQLite", error)
+        # finally:
+        #     if not (db_connection.in_transaction):
+        #         db_connection.close()
+        #         print("sqlite connection is closed")
 
         if debugflag:
-          debug()
-        
-        open_db(self.database)
-        
+            self.debug()
+    
+    def open_db(self, database_file):
+        connection = sqlite3.connect(database_file) 
+        cursor = connection.cursor()
+        cursor.execute("PRAGMA database_list;")
+        curr_table = cursor.fetchall()
+        print(curr_table)
+        return connection, cursor
 
+    def create_table(self, database_cursor):
+      """Creates a table called notecard_table in the database with the appropriate columns.
+      """
+      database_cursor.execute('''
+          CREATE TABLE ClipsDB (
+              id        INTEGER     PRIMARY KEY     NOT NULL,
+              type      INTEGER     NOT NULL,
+              created   TEXT        NOT NULL        DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW', 'localtime')),
+              accessed  TEXT        NOT NULL        DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW', 'localtime')),
+              uri       TEXT,
+              data      BLOB,
+              checksum  STRING      NOT NULL        UNIQUE
+          );
+          ''')
+      
+    def debug(self):
+        label = Gtk.Label()
+        image = Gtk.Image.new_from_icon_name("image-x-generic", Gtk.IconSize.DIALOG)
+        window = Gtk.Window(title="Clips Debug Window") #debug window to see contents displayed in Gtk.Window
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        box.pack_start(image, True, True, 0)
+        box.pack_start(label, True, True, 0)
+        window.set_border_width(6)
+        window.add(box)
+        window.show_all()
+        window.connect("destroy", Gtk.main_quit)
+        # just for debugging at CLI to enable CTRL+C quit
+        GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, signal.SIGINT, Gtk.main_quit)
 
 clips = ClipsStore()
 Gtk.main()
