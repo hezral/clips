b901967940a4ddbd85898c2d605b584d2fb8bd79
Adi Hezral <hezral@gmail.com>
2021-04-03 15:10:05 +0800

    add Alt+Left/Right navigation for Clips/Settings

--- src/application.py
+++ src/application.py
@@ -66,8 +66,8 @@ class Clips(Gtk.Application):
         self.setup_action("hide", self.on_hide_action, "Escape")
         self.setup_action("quit", self.on_quit_action, "<Ctrl>Q")
         self.setup_action("search", self.on_search_action, "<Ctrl>F")
-        # self.setup_action("settings-view", self.on_switch_views, "<Shift>S")
-        # self.setup_action("clips-view", self.on_switch_views, "<Shift>C")
+        self.setup_action("settings-view", self.on_switch_views, "<Alt>Right")
+        self.setup_action("clips-view", self.on_switch_views, "<Alt>Left")
 
         # selected clip actions
         # self.setup_action("protect", self.on_clip_actions, "<Alt>P")
@@ -180,14 +180,15 @@ class Clips(Gtk.Application):
                 clips_container.on_clip_action(action=action.props.name)
 
     def on_switch_views(self, action, param):
-        
         if self.main_window is not None:
             if action.props.name == "settings-view":
-                print("settings-view")
-                # self.main_window.stack.set_visible_child_name("settings-view")
+                self.main_window.settings_view.show_all()
+                self.main_window.stack.set_visible_child_name("settings-view")
+                self.main_window.view_switch.props.active = True
             if action.props.name == "clips-view":
-                print("clips-view")
-                # self.main_window.stack.set_visible_child_name("clips-view")
+                self.main_window.clips_view.show_all()
+                self.main_window.stack.set_visible_child_name("clips-view")
+                self.main_window.view_switch.props.active = False
 
     def on_hide_action(self, action, param):
         if self.main_window is not None:
