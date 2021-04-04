b4cb8bed3081aa30dec795c2abd18b9dc5b3c4f9
Adi Hezral <hezral@gmail.com>
2021-04-04 10:13:54 +0800

    fixed exception handling for GetAppInfo

--- src/utils.py
+++ src/utils.py
@@ -49,22 +49,23 @@ def RunAsync(func):
 
 ###################################################################################################################
 
-def GetAppInfo(app_name):
+def GetAppInfo(app):
     from gi.repository import Gio
     all_apps = Gio.AppInfo.get_all()
     try:
-        appinfo = [child for child in all_apps if child.get_name() == app_name][0]
+        appinfo = [child for child in all_apps if child.get_name() == app][0]
     except:
         appinfo = None
     finally:
         if appinfo is not None:
             icon = appinfo.get_icon()
             if icon is not None:
-                icon_name = icon.to_string()
-            else:
-                icon_name = "application-default-icon"
-        app_name = appinfo.get_name()
-        return app_name, icon_name
+                icon_name = icon.to_string()   
+            app_name = appinfo.get_name()
+        else:
+            app_name = app
+            icon_name = "application-default-icon"
+    return app_name, icon_name
 
 def GetWidgetByName(widget, child_name, level, doPrint=False):
     '''
