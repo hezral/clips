bb26e96fa372c0ae1fd9bee6efe49a8a85e006a2
Adi Hezral <hezral@gmail.com>
2021-05-28 12:37:26 +0800

    renamed get_active_appinfo_xlib

--- src/utils.py
+++ src/utils.py
@@ -354,7 +354,7 @@ def get_active_app_window_xprop():
 
     return source_app, source_icon
 
-def get_active_app_window_xlib():
+def get_active_appinfo_xlib():
     source_app = None
     source_icon = None
     all_apps = get_all_apps()
