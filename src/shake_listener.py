f850e5ce41c06c9902320198d9a35503e6f8cb4e
Adi Hezral <hezral@gmail.com>
2021-10-15 22:37:15 +0800

    add error handling

--- src/shake_listener.py
+++ src/shake_listener.py
@@ -75,42 +75,43 @@ class ShakeListener():
             self.mouse_pressed = False
 
     def detect_mouse_movement(self, x, y):
-        if not self.app.main_window.is_visible():
-            # state = "is_shaking:{0}, shake_count:{1}, showing:{2}".format(self.isShaking, self.shake_count, self.showing)
-            # details1 = "now_x:{0}, old_x:{1}".format(self.now_x, self.old_x)
-            # details2 = "min_x:{0}, max_x:{1}".format(self.min_x, self.max_x)
-            # details3 = "has_min:{0}, has_max:{1}".format(self.has_min, self.has_max)
-            # details4 = "showing_timestamp:{0}".format(self.showing_timestamp)
-            # details5 = "shake_timeout_timestamp:{0}".format(self.shake_timeout_timestamp)
-            # details6 = "shake_timeout_timestamp_diff:{0}".format(self.shake_timeout_timestamp_diff)
-            # details7 = "shake_slice_timestamp:{0}".format(self.shake_slice_timestamp)
-            # details8 = "shake_slice_timestamp_diff:{0}".format(self.shake_slice_timestamp_diff)
-            # details9 = "self.shake_timeout_timestamp_diff >= SHAKE_TIMEOUT:{0}".format(self.shake_timeout_timestamp_diff >= SHAKE_TIMEOUT)
-            # details10 = "self.shake_slice_timestamp_diff >= SHAKE_SLICE_TIMEOUT:{0}".format(self.shake_slice_timestamp_diff >= SHAKE_SLICE_TIMEOUT)
-            # details11 = "self.max_x-self.min_x > SHAKE_DIST:{0}".format(self.max_x-self.min_x > SHAKE_DIST)
-            # update = "{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}\n{9}\n{10}\n{11}\n".format(state, details1, details2, details3, details4, details5, details6, details7, details8, details9, details10, details11)
-            # update = state
-            # print(update)
-            # print(x,y)
+        if self.app.main_window is not None:
+            if not self.app.main_window.is_visible():
+                # state = "is_shaking:{0}, shake_count:{1}, showing:{2}".format(self.isShaking, self.shake_count, self.showing)
+                # details1 = "now_x:{0}, old_x:{1}".format(self.now_x, self.old_x)
+                # details2 = "min_x:{0}, max_x:{1}".format(self.min_x, self.max_x)
+                # details3 = "has_min:{0}, has_max:{1}".format(self.has_min, self.has_max)
+                # details4 = "showing_timestamp:{0}".format(self.showing_timestamp)
+                # details5 = "shake_timeout_timestamp:{0}".format(self.shake_timeout_timestamp)
+                # details6 = "shake_timeout_timestamp_diff:{0}".format(self.shake_timeout_timestamp_diff)
+                # details7 = "shake_slice_timestamp:{0}".format(self.shake_slice_timestamp)
+                # details8 = "shake_slice_timestamp_diff:{0}".format(self.shake_slice_timestamp_diff)
+                # details9 = "self.shake_timeout_timestamp_diff >= SHAKE_TIMEOUT:{0}".format(self.shake_timeout_timestamp_diff >= SHAKE_TIMEOUT)
+                # details10 = "self.shake_slice_timestamp_diff >= SHAKE_SLICE_TIMEOUT:{0}".format(self.shake_slice_timestamp_diff >= SHAKE_SLICE_TIMEOUT)
+                # details11 = "self.max_x-self.min_x > SHAKE_DIST:{0}".format(self.max_x-self.min_x > SHAKE_DIST)
+                # update = "{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}\n{9}\n{10}\n{11}\n".format(state, details1, details2, details3, details4, details5, details6, details7, details8, details9, details10, details11)
+                # update = state
+                # print(update)
+                # print(x,y)
 
-            self.now_x = x
-            if self.now_x < self.old_x:
-                if self.has_min == 0:
-                    self.has_min = 1
-                    self.min_x = self.now_x
-                else:
-                    self.min_x = min(self.min_x, self.now_x)
+                self.now_x = x
+                if self.now_x < self.old_x:
+                    if self.has_min == 0:
+                        self.has_min = 1
+                        self.min_x = self.now_x
+                    else:
+                        self.min_x = min(self.min_x, self.now_x)
 
-            elif self.now_x > self.old_x:
-                if self.has_max == 0:
-                    self.has_max = 1
-                    self.max_x = self.now_x
-                else:
-                    self.max_x = max(self.max_x, self.now_x)
+                elif self.now_x > self.old_x:
+                    if self.has_max == 0:
+                        self.has_max = 1
+                        self.max_x = self.now_x
+                    else:
+                        self.max_x = max(self.max_x, self.now_x)
 
-            self.old_x = self.now_x
+                self.old_x = self.now_x
 
-            self.is_shaking()
+                self.is_shaking()
 
     def is_shaking(self):
         self.isShaking = False
