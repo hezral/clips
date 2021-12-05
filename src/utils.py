c618dc2bd4c1e2651cf668fa4698a589933199fd
Adi Hezral <hezral@gmail.com>
2021-12-05 14:23:33 +0800

    add decorator for logging execution time

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,43 @@ from datetime import datetime
 
 #-------------------------------------------------------------------------------------------------------
 
+from datetime import timedelta
+from functools import wraps
+from timeit import default_timer as timer
+from typing import Any, Callable, Optional
+
+def metrics(func: Optional[Callable] = None, name: Optional[str] = None, hms: Optional[bool] = False) -> Any:
+    """Decorator to show execution time.
+
+    :param func: Decorated function
+    :param name: Metrics name
+    :param hms: Show as human-readable string
+    """
+    assert callable(func) or func is None
+
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            comment = f"{datetime.now()} execution time of {name or fn.__name__}:"
+            t = timer()
+            result = fn(*args, **kwargs)
+            te = timer() - t
+
+            # # Log metrics
+            # from common import log
+            # logger = log.withPrefix('[METRICS]')
+            if hms:
+                print(f"{comment} {timedelta(seconds=te)}")
+            else:
+                print(f"{comment} {te:>.6f} sec")
+
+            return result
+        return wrapper
+
+    return decorator(func) if callable(func) else decorator
+
+#-------------------------------------------------------------------------------------------------------
+
 def run_async(func):
     '''
     https://github.com/learningequality/ka-lite-gtk/blob/341813092ec7a6665cfbfb890aa293602fb0e92f/kalite_gtk/mainwindow.py
